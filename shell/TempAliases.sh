0812d8e7d7d9ec7bc5b421888e18b7e3762712bc
zia.khalid <zia.khalid@ZiaMac2-FD2020MLT044-0907.local>
2021-05-17 12:06:13 +0800

    aliases update

--- shell/TempAliases.sh
+++ shell/TempAliases.sh
@@ -1,8 +1,13 @@
-#Movement among Directories
 
+#Move to Directory
 alias mthll="cd ~/Documents/Root/GIT_REPO/LALAMOVE/LLMGlobalDriver/dapp-android-component"
+alias mtMyNotes="cd  ~/Documents/Root/GIT_REPO/MyNotes"
+alias mtDotfilesShell="cd ~/dotfiles/shell"
+alias mtDotfilesShellScript="cd  ~/dotfiles/shell/script"
 
-alias mtdfs="cd ~/dotfiles/shell"
+#Edit files
+alias standup="vim ~/Documents/Root/GIT_REPO/MyNotes/Office/StandUp"
+alias testingllm="vim ~/Documents/Root/GIT_REPO/MyNotes/Office/TestingLalamove"
+alias todo="vim ~/Documents/Root/GIT_REPO/MyNotes/ToDo"
+alias editTempAliases="vim ~/dotfiles/shell/TempAliases.sh"
 
-alias mtve="cd  ~/Documents/Root/GIT_REPO/VimEdit"	
-alias mtmn="cd  ~/Documents/Root/GIT_REPO/MyNotes"
