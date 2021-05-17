7c467b1cfd3ca2da489c5642c8e33428e24f7586
zia.khalid <zia.khalid@ZiaMac2-FD2020MLT044-0907.local>
2021-05-17 15:07:07 +0800

    TempAliases update

--- shell/TempAliases.sh
+++ shell/TempAliases.sh
@@ -1,13 +1,37 @@
+#Path
+hllPath="~/Documents/Root/GIT_REPO/LALAMOVE/LLMGlobalDriver/dapp-android-component"
+notesPath="~/Documents/Root/GIT_REPO/MyNotes"
+notesLearningPath="$notesPath/Learning"
+notesOfficePath="$notesPath/Office"
+notesOtherPath="$notesPath/Other"
+notesPersonalPath="$notesPath/Personal"
+notesToolsPath="$notespath/Tools"
+dotfilesPath="~/dotfiles"
+dotfilesShellPath="$dotfilesPath/shell"
+dotfilesShellScriptPath="$dotfilesPath/shell/script"
+productivityScriptPath="~/Documents/Root/GIT_REPO/ProductivityScripts"
+productivityScriptShellPath="$productivityScriptPath/Shell"
 
 #Move to Directory
-alias mthll="cd ~/Documents/Root/GIT_REPO/LALAMOVE/LLMGlobalDriver/dapp-android-component"
-alias mtMyNotes="cd  ~/Documents/Root/GIT_REPO/MyNotes"
-alias mtDotfilesShell="cd ~/dotfiles/shell"
-alias mtDotfilesShellScript="cd  ~/dotfiles/shell/script"
+alias mthll="cd $hllPath"
+alias mtMyNotes="cd $notesPath" 
+alias mtOffice="cd $notesOfficePath"
+alias mtDotfilesShell="cd $dotfilesShellPath"	
+alias mtDotfilesShellScript="cd $dotfilesShellScriptPath"
+alias mtProductivityScript="cd $productivityScriptPath"
+alias mtProductivityScriptShell="cd $productivityScriptShellPath"
 
 #Edit files
-alias standup="vim ~/Documents/Root/GIT_REPO/MyNotes/Office/StandUp"
-alias testingllm="vim ~/Documents/Root/GIT_REPO/MyNotes/Office/TestingLalamove"
-alias todo="vim ~/Documents/Root/GIT_REPO/MyNotes/ToDo"
-alias editTempAliases="vim ~/dotfiles/shell/TempAliases.sh"
+alias editStandup="vim $notesOfficePath/StandUp"
+alias editTestingllm="vim $notesOfficePath/TestingLalamove"
+alias editTodo="vim $notesPath/ToDo" 
+alias editTempAliases="vim $dotfilesShellPath/TempAliases.sh"
+alias editImportantCommands="vim $notesLearningPath/ImportantCommands"
+alias editSprintTask="vim $notesOfficePath/SprintTask"
+alias editSprintTaskNotes="vim $notesOfficePath/SprintTaskNotes"
+
+#Show files
+alias showTodo="cat $notesPath/ToDo"
+alias showSprintTaskNotes="cat $notesOfficePath/SprintTaskNotes"
+alias showImportantCommands="cat $notesLearningPath/ImportantCommands"
 
